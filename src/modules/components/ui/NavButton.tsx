03d7c6415e2c3d6eb98dbcf8925230d43691f799
CrispyMiatr <maxivowes@gmail.com>
2025-12-19 01:30:17 +0100

    feat(NavButton): change Link to NavLink

--- src/modules/components/ui/NavButton.tsx
+++ src/modules/components/ui/NavButton.tsx
@@ -1,15 +1,12 @@
-import { Link, useLocation } from 'react-router';
+import { NavLink } from 'react-router';
 import type { NavButtonType } from '~/shared';
 import styles from '~styles/app.module.scss';
 
 export const NavButton = ({ name, link }: NavButtonType) => {
-    // Check if the current location matches the button's link
-    const location = useLocation();
-    const isActive = location.pathname === link;
 
     return (
-        <Link to={link} className={`${styles['nav-button']} ${isActive ? styles['nav-button--active'] : ''}`}>
-            <p className={styles['nav-button__title']}>{name}</p>
-        </Link>
+        <NavLink to={link} className={({ isActive }) => isActive ? `${styles['nav-button']} ${styles['nav-button--active']}` : styles['nav-button']}>
+            {name}
+        </NavLink>
     );
 };
