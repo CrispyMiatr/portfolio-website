386ddaf7bd8aa538c03d7974d1c07c36256ad259
CrispyMiatr <maxivowes@gmail.com>
2025-12-19 01:32:17 +0100

    feat(Footer): add Footer

--- src/modules/components/layout/Footer.tsx
+++ src/modules/components/layout/Footer.tsx
@@ -1,10 +1,35 @@
+import { Link } from 'react-router';
+import { Icons, NavButton } from '../ui';
 import styles from '~styles/app.module.scss';
 
 export const Footer = () => {
 
     return (
         <nav className={styles['footer']}>
+            <Link to="/">
+                <img src={'./src/assets/logo.svg'} alt="Logo" className={styles['footer__logo']} />
+            </Link>
 
+            <div className={styles['footer__nav-buttons']}>
+                <NavButton name="Home" link="/" />
+                <NavButton name="Projects" link="/projects" />
+                <NavButton name="About" link="/about" />
+                <NavButton name="Contact" link="/contact" />
+            </div>
+
+            <div className={styles['footer__socials']}>
+                <a href="" target='_blank' className={styles['footer__socials__icon']}>
+                    <Icons.Mail aria-label="Send email" />
+                </a>
+
+                <a href="" target='_blank' className={styles['footer__socials__icon']}>
+                    <Icons.LinkedIn aria-label="LinkedIn profile" />
+                </a>
+
+                <a href="" target='_blank' className={styles['footer__socials__icon']}>
+                    <Icons.Instagram aria-label="Instagram profile" />
+                </a>
+            </div>
         </nav>
     );
 };
