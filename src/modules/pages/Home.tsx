b9a858f47d3c14b5038d299e7fc54c80d82df835
CrispyMiatr <maxivowes@gmail.com>
2025-12-20 01:02:49 +0100

    feat(Home): add background logo

--- src/modules/pages/Home.tsx
+++ src/modules/pages/Home.tsx
@@ -1,10 +1,12 @@
+import { Button } from '~/components';
 import styles from '~styles/pages/home.module.scss';
 
 export const Home = () => {
 
     return (
         <div className={styles['home-container']}>
-            <h1>Welcome!</h1>
+            {/* <div className={styles['home-container__bg']} /> */}
+            <img className={styles['home-container__bg']} src="./src/assets/logo.svg" alt="" />
         </div>
     );
 };
